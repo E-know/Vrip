65df7f4dd86217afc4eef59f44e473ac892005bf
Inho Choi <eknow25@kakao.com>
2023-03-05 22:37:56 +0900

    Init Project

---
+++ Vrip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vrip
+//
+//  Created by Inho Choi on 2023/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
